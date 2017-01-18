5e4fd0874c5122186b2211d0c9df0fa67abd1343
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-18 11:03:46 +0000

    migrations commited

--- models/category.js
+++ models/category.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Category = sequelize.define('Category', {
-    name: DataTypes.STRING,
+    name: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Category.belongsToMany(Story, {through: 'StoryCategory'});
+        Category.belongsToMany(Story, {through:'StoryCategory'})
+
       }
     }
   });
