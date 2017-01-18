2de9efa98978b8ccee67fb758c91cf34db55ec49
Agata Sumowska <agata.sumowska@gmail.com>
2017-01-18 14:10:18 +0000

    Adding ref about models to bin/www

--- models/category.js
+++ models/category.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Category.belongsToMany(Story, {through: 'StoryCategory'});
+        Category.belongsToMany(models.Story, {through: 'StoryCategory'});
       }
     }
   });
