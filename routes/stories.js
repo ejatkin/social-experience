be2b1dba15b640540bb9bd32a93624eb73e3fdfc
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-19 15:11:53 +0000

    adding db to routes

--- routes/stories.js
+++ routes/stories.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (request, result, next) {
-  result.render('stories/index');
-  // result.send('this is the list of all stories');
-});
+router.get('/', function (req, res, next) {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    console.log(result[0])
+    res.render('stories/index', { title: 'Social Experience' , quotes: result});
+  });
 
 router.get('/new', function(request, result, next){
   result.render('stories/new')
@@ -16,8 +18,27 @@ router.get('/:id', function(request, result, next){
   // result.send('this is a display page for a story with id: ' + request.params.id )
 });
 
-router.post('/',function(request, result, next){
+router.post('/',function(req, res, next){
   // create story object
-  router.redirect('/');
+  db.collection('quotes').save(req.body, (err, result) => {
+
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  });
 });
 module.exports = router;
+
+
+
+app.post('/quotes', (req, res) => {
+
+  db.collection('quotes').save(req.body, (err, result) => {
+
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
