cd5d198161abbb22da5ba3fecf818e7c8cc61748
George Seeger <georgeseeger@hotmail.com>
2017-01-19 11:54:23 +0000

    Routing linking to jade files

--- routes/stories.js
+++ routes/stories.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (request, result, next) {
-  // result.render('story/index');
-  result.send('this is the list of all stories');
+  result.render('stories/index');
+  // result.send('this is the list of all stories');
 });
 
 router.get('/new', function(request, result, next){
-  // result.render('story/new')
-  result.send('this is a form for new stories');
+  result.render('stories/new')
+  // result.send('this is a form for new stories');
 });
 
 router.get('/:id', function(request, result, next){
-  // result.render('story/view')
-  result.send('this is a display page for a story with id: ' + request.params.id )
+  result.render('stories/view', {storyId: request.params.id})
+  // result.send('this is a display page for a story with id: ' + request.params.id )
 });
 
 router.post('/',function(request, result, next){
   // create story object
-
+  router.redirect('/');
 });
 module.exports = router;
